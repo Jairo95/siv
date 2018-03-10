50c22d7eed142986265dde10af8d72b137d61a5c
Jairo95 <jairotft@gmail.com>
2018-03-10 12:20:32 -0500

    Basic models

--- siv/forum/models.py
+++ siv/forum/models.py
@@ -1,2 +1,47 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class WorryUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    id = models.BigIntegerField(primary_key=True, unique=True, auto_created=True)
+    user_name = models.CharField(max_length=100)
+
+
+class WorryCategory(models.Model):
+    id = models.BigIntegerField(primary_key=True, unique=True, auto_created=True)
+    name = models.CharField(max_length=100)
+
+
+class WorryType(models.Model):
+    id = models.BigIntegerField(primary_key=True, unique=True, auto_created=True)
+    name = models.CharField(max_length=100)
+
+
+class UserAction(models.Model):
+    id = models.BigIntegerField(primary_key=True, unique=True, auto_created=True)
+    worry_user = models.ForeignKey(WorryUser, on_delete=models.CASCADE)
+
+
+class WorryMessage(models.Model):
+    id = models.BigIntegerField(primary_key=True, unique=True, auto_created=True)
+    worry_category = models.ForeignKey(WorryCategory, on_delete=models.CASCADE)
+    worry_type = models.ForeignKey(WorryType, on_delete=models.CASCADE)
+    user_action = models.ForeignKey(UserAction, on_delete=models.CASCADE)
+
+
+class OpinionMessage(models.Model):
+    id = models.BigIntegerField(primary_key=True, unique=True, auto_created=True)
+    user_action = models.ForeignKey(UserAction, on_delete=models.CASCADE)
+
+
+class MessageRecord(models.Model):
+    worry_message = models.ForeignKey(WorryMessage, on_delete=models.CASCADE)
+    opinion_message = models.ForeignKey(OpinionMessage, on_delete=models.CASCADE)
+
+
+class ConfigurationPreference(models.Model):
+    worry_user = models.ForeignKey(WorryUser, on_delete=models.CASCADE)
+    worry_category = models.ForeignKey(WorryCategory, on_delete=models.CASCADE)
+
 
--- siv/siv/settings.py
+++ siv/siv/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'forum'
+    'forum.apps.ForumConfig'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'siv.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/'
